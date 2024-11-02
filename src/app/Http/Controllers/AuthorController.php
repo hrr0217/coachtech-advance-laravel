357e4617d8953d7c686d971a2c98494f4d51fbe0
haruna <neotokyo147@gmail.com>
2024-11-03 00:46:50 +0900

    blade

---
+++ src/app/Http/Controllers/AuthorController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Author;
+
+class AuthorController extends Controller
+{
+    public function add(){
+        return view('add');
+    }
+
+    public function create(Request $request){
+        $form = $request->all();
+        Author::create($form);
+        return redirect('/');
+    }
+
+    public function index()
+    {
+        $authors = Author::all();
+        return view('index', ['authors' => $authors]);
+    }
+}
